681c0cdc521509a1f61f225b05956e44f5362ce5
Jungmin Kim <kjm99d@naver.com>
2025-04-12 21:31:32 +0900

    libNetwork.dll 의 기능 중 내부망 IP 주소를 가져올 수 있는 함수 추가 (#1)

--- libNetwork/libNetwork.h
+++ libNetwork/libNetwork.h
@@ -6,8 +6,12 @@
 
 #include <windows.h>
 
+
+
 // 도메인 정보를 기반으로 Public IP 정보를 획득한다.
 DLL_EXPORT int ResolveDomainToSockAddr(const char* domain, unsigned short port, char * pRemoteIP, int nLenRemoteIP);
+
+DLL_EXPORT int GetLocalNetwork(OUT char* pLocalIP, int nLenLocalIP);
 
 // STUN 서버에 요청하여 사용자의 공인 IP/PORT 정보를 획득한다.
 DLL_EXPORT int GetPublicNetwork(OUT char* pPublicIP, int nLenPublicIP, OUT int* nPublicPort, char* pRemoteIP, int nLenRemoteIP, int nRemotePort);
